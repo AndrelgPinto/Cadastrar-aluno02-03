cce6ab0b22aa26d80a8fae168174563b0df15bc0
andre.lgpinto <andregomeees0@gmail.com>
2023-03-02 21:32:43 -0300

    segundocommit

--- Cadastrar-aluno02-03/Program.cs
+++ Cadastrar-aluno02-03/Program.cs
@@ -1,21 +1,22 @@
 ﻿string[,] cadastrar = new string[5, 7];
-int  numerodeaulas = 0 , numerofaltas = 0;
-int notaprova1 = 0, notadaprova2 = 0, notadaprova3 = 0, notadotrabalho = 0;
+
 int i = 0;
 string nomealuno = " ";
+int linha = 0;
+int coluna = 0; 
 
 for (i = 0; i <= 4; i++)
 {
     Console.Clear();
     Console.WriteLine("Qual nome do Aluno");
-    nomealuno = Console.ReadLine();
+    cadastrar[linha, coluna]= Console.ReadLine();
 
     Console.Clear();
     Console.WriteLine("Numero de aulas dadas");
-    numerodeaulas = int.Parse(Console.ReadLine());
+    cadastrar[linha, coluna] = Console.ReadLine();
 
 
     Console.Clear();
     Console.WriteLine("Numero de Faltas");
-    numerodeaulas = int.Parse(Console.ReadLine());
+    cadastrar[linha, coluna] = Console.ReadLine();
 }
