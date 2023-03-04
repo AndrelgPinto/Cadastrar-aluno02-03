c6b0d7c69b489176c61d00d00b7f10d8f6e56e06
andre.lgpinto <andregomeees0@gmail.com>
2023-03-03 22:06:26 -0300

    dificil

--- Cadastrar-aluno02-03/Program.cs
+++ Cadastrar-aluno02-03/Program.cs
@@ -1,16 +1,21 @@
-﻿string[,] aluno = new string[5, 7];
+﻿string aluno;
+int faltasfinais = 0;
 int aulas = 0;
 int falta = 0;
-int linha, coluna = 0;
+double media = 0;
 double prova1, prova2, prova3, trabalho;
-int i = 0;
+double vale1 = 0.3;
+double vale2 = 0.25;
+double vale3 = 0.35;
+double presenca = 0;
+int i;
 string resultado = "";
 
-for (linha = 0; linha <= 4; linha++)
+for (i = 1; i <= 5; i++)
 {
     Console.Clear();
-    Console.WriteLine("Nome do Aluno");
-    aluno[linha, coluna++] = Console.ReadLine();
+    Console.WriteLine("Nome do " + i + "º Aluno");
+    aluno = Console.ReadLine();
 
     Console.WriteLine("Numero de Aulas");
     aulas = int.Parse(Console.ReadLine());
@@ -19,18 +24,41 @@ for (linha = 0; linha <= 4; linha++)
     falta = int.Parse(Console.ReadLine());
 
     Console.WriteLine("Qual nota da prova 1"  );
-    prova1 = int.Parse(Console.ReadLine());
+    prova1 = double.Parse(Console.ReadLine());
 
     Console.WriteLine("Qual nota da prova 2" );
-    prova2 = int.Parse(Console.ReadLine());
+    prova2 = double.Parse(Console.ReadLine());
 
     Console.WriteLine("Qual nota da prova 3" );
-    prova3 = int.Parse(Console.ReadLine());
+    prova3 = double.Parse(Console.ReadLine());
 
     Console.WriteLine("Qual nota do Trabalho");
-    trabalho = int.Parse(Console.ReadLine());
+    trabalho = double.Parse(Console.ReadLine());
 
-    Console.WriteLine(  aluno + "resultado " + linha, coluna == aulas + falta + prova1 + prova2 + prova3);
+    media = (prova1 * vale1 + prova2 * vale2 + prova3 * vale3) + trabalho;
+    faltasfinais = (falta * 100) / aulas;
+    presenca = 100 - faltasfinais;
+
+
+    if (media >= 6 && presenca > 25)
+    {
+        Console.WriteLine(aluno + " Aprovado com " + media.ToString("F") + "de media e " + presenca + " de presenca");
+    }
+    if (media < 6 && presenca >= 25)
+    {
+        Console.WriteLine(aluno + " Reprovado por menção com " + media.ToString("F") + " de media");
+    }
+    if (media > 6 && presenca < 25)
+    {
+       Console.WriteLine(aluno + " Reprovado por menção com " + presenca + " de presença");
+    }
+    if (media < 6 && presenca < 25)
+    {
+        Console.WriteLine(aluno + " Reprovadopor menção e faltas com " + media.ToString("F") + " de media e " + presenca + " de menção");
+    }
+
+    Console.WriteLine("Pressione ENTER para continuar");
+    Console.ReadLine();
 }
 
  
